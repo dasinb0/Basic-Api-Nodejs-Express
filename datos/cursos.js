855e7063da9ea7e6ef75f59b79053f99854e8e2f
dasinb0 <94384375+dasinb0@users.noreply.github.com>
2023-09-02 04:04:37 -0300

    first commit

---
+++ datos/cursos.js
@@ -0,0 +1,51 @@
+let infoCursos = {
+    programacion:[
+        {
+        id: 1,
+        titulo: 'Aprende python',
+        lenguaje: 'python',
+        vistas: 15000,
+        nivel: 'basico'
+         },
+         {
+            id: 2,
+            titulo: 'Aprende python intermedio',
+            lenguaje: 'python',
+            vistas: 150000,
+            nivel: 'intermedio'
+             },
+             {
+                id: 3,
+                titulo: 'Aprende javascript',
+                lenguaje: 'javascript',
+                vistas: 1500000,
+                nivel: 'basico'
+                 }
+             
+    ],
+    matematicas:[
+        {
+            id: 1,
+            titulo: 'Aprende algebra',
+            tema: 'algebra',
+            vistas: 15000000,
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo: 'Aprende geometria',
+            tema: 'geometria',
+            vistas: 150000000,
+            nivel: 'basico'
+        },
+        {
+            id: 3,
+            titulo: 'Aprende fracciones',
+            tema: 'fracciones',
+            vistas: 12,
+            nivel: 'experto'
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
